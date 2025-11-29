f6d5f88627267490f9b429c5179c6aa54ee7ee6a
marrkobacsi <81500230+marrkoacsi@users.noreply.github.com>
2025-11-29 14:58:21 +0100

    Remove redundant API_BASE declaration

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,9 +1,6 @@
 const API_BASE =
   import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
-const API_BASE =
-  import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
-
 // REGISZTRÁCIÓ
 export async function register(payload) {
   const res = await fetch(`${API_BASE}/api/auth/register/`, {
