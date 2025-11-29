a4610e6a6a0f172aa2745ae5f3f4181cffb75e43
marrkobacsi <81500230+marrkoacsi@users.noreply.github.com>
2025-11-29 22:38:28 +0100

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,7 @@ from accounts.views import RegisterView, LoginView, VerifyEmailView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("api/auth/register/", RegisterView.as_view(), name="register"),
     path("api/auth/login/", LoginView.as_view(), name="login"),
     path("api/auth/verify-email/", VerifyEmailView.as_view(), name="verify-email"),
